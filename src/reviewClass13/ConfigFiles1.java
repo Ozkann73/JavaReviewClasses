d133b602c756d2dad28408797419825cb97cf4aa
Ozkan Kara <ozzyblackk73@gmail.comgit config --global user.email ozzyblackk73@gmail.com>
2022-08-01 18:21:13 -0700

    reviewclass12-13  FileReading and ExceptionHandling

---
+++ src/reviewClass13/ConfigFiles1.java
@@ -0,0 +1,16 @@
+package reviewClass13;
+
+import utils.ConfigReader;
+
+import java.util.Properties;
+
+public class ConfigFiles1 {
+    public static void main(String[] args) {
+        Properties properties = ConfigReader.read("Files/Config.properties");
+        System.out.println(properties.getProperty("user"));
+        System.out.println(properties.getProperty("password"));
+        System.out.println(properties.getProperty("URL"));
+
+
+    }
+}
---
+++ src/reviewClass13/ExcelFileDemo1.java
@@ -0,0 +1,33 @@
+package reviewClass13;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ExcelFileDemo1 {
+    public static void main(String[] args) {
+        String path = "Files/ExcelTest.xlsx";
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(path);
+            XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fileInputStream);
+            Sheet sheet =xssfWorkbook.getSheet("Sheet1");
+            int noOfRows = sheet.getPhysicalNumberOfRows();
+            for (int rowNo=0; rowNo<noOfRows; rowNo++){
+                Row row = sheet.getRow(rowNo);
+                int noOfCells = row.getPhysicalNumberOfCells();
+                for (int cellNo=0; cellNo<noOfCells; cellNo++){
+                    System.out.print(row.getCell(cellNo)+" ");
+                }
+                System.out.println();
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
